624632e904ebea6dea6c7a982e0f070b775795c7
h8mastre <h8mastre@gmail.com>
2013-12-30 03:52:40 +0000

    ReadInt64バグ修正。 BEW個人最適化。 FPWriteExtテスト済み。

--- BER.cs
+++ BER.cs
@@ -17,16 +17,16 @@ namespace AFPt {
         public byte ReadByte() { return br.ReadByte(); }
         public sbyte ReadSByte() { return br.ReadSByte(); }
         public short ReadInt16() {
-            int v = br.ReadByte() << 8;
-            v |= br.ReadByte();
+            int v = br.ReadByte();
+            v <<= 8; v |= br.ReadByte();
             return (short)v;
         }
         public ushort ReadUInt16() { return (ushort)ReadInt16(); }
         public int ReadInt32() {
-            int v = br.ReadByte() << 24;
-            v |= br.ReadByte() << 16;
-            v |= br.ReadByte() << 8;
-            v |= br.ReadByte();
+            int v = br.ReadByte();
+            v <<= 8; v |= br.ReadByte();
+            v <<= 8; v |= br.ReadByte();
+            v <<= 8; v |= br.ReadByte();
             return v;
         }
         public uint ReadUInt32() { return (uint)ReadInt32(); }
@@ -34,15 +34,15 @@ namespace AFPt {
 
         public ulong ReadUInt64() { return (ulong)ReadInt64(); }
         public long ReadInt64() {
-            int v = br.ReadByte() << 24;
-            v |= br.ReadByte() << 16;
-            v |= br.ReadByte() << 8;
-            v |= br.ReadByte();
-            int w = br.ReadByte() << 24;
-            w |= br.ReadByte() << 16;
-            w |= br.ReadByte() << 8;
-            w |= br.ReadByte();
-            return (long)(v << 32) | (long)w;
+            Int64 v = br.ReadByte();
+            v <<= 8; v |= br.ReadByte();
+            v <<= 8; v |= br.ReadByte();
+            v <<= 8; v |= br.ReadByte();
+            v <<= 8; v |= br.ReadByte();
+            v <<= 8; v |= br.ReadByte();
+            v <<= 8; v |= br.ReadByte();
+            v <<= 8; v |= br.ReadByte();
+            return v;
         }
     }
 }
